3c3028ea49fec7eadcbc718a437ed9a1a912fd90
brleblanc98 <brleblanc98@gmail.com>
2023-03-30 20:05:11 -0400

    Update UI to new counter; Not connected to any DB

--- app.vue
+++ app.vue
@@ -1,8 +1,69 @@
 <template>
-  <div class="content">
-    <h2 class="text_shadows">HEY MACKIE. YOU LOST THE GAME :)</h2>
+  <div class='content'>
+    <span class='text_no_shadows'>Times Mackie has lost the game</span>
+    <span class='text_shadows'>{{ count }}</span>
+    <button class='inc_button' @click='handleButton' :disabled='!isAvailable'>
+      <span class='inc_button_available' v-if='isAvailable'>
+        Press here if Mackie lost the game
+      </span>
+      <span class='inc_button_unavailable' v-else>
+        <span>{{ minutes }}m {{ seconds }}s</span>
+      </span>
+    </button>
   </div>
 </template>
+
+<script lang='ts'>
+export default {
+  head() {
+    return {
+      title: 'Mackie Lost The Game'
+    }
+  },
+  data() {
+    return {
+      count: 0,
+      countDownDate: new Date(),
+      minutes: 0,
+      seconds: 0,
+      isAvailable: false
+    }
+  },
+  mounted() {
+    setInterval(() => {
+      this.updateTimer();
+    }, 1000);
+  },
+  methods: {
+    updateTimer() {
+      if (!this.isAvailable) {
+        const now = new Date().getTime();
+        const distance = this.countDownDate.getTime() - now;
+
+        if (distance < 0) {
+          this.minutes = 0;
+          this.seconds = 0;
+          this.isAvailable = true;
+        } else {
+          this.minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+          this.seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        }
+      }
+    },
+    setDateToNowPlus30m() {
+      const now = new Date();
+      this.countDownDate = new Date(now.getTime() + 30*60000);
+      // this.countDownDate = new Date(now.getTime() + 10000);
+    },
+    handleButton() {
+      this.count++;
+      this.isAvailable = false;
+      this.setDateToNowPlus30m();
+      this.updateTimer();
+    }
+  }
+}
+</script>
 
 <style lang='scss'>
 *,
@@ -35,6 +96,36 @@ body {
   display: flex;
   align-content: center;
   justify-content: center;
+  flex-direction: column;
+  text-align: center;
+}
+
+.inc_button {
+  font-family: bungee, sans-serif;
+  font-weight: 400;
+  font-size: calc(2rem + 3vw);
+  background-color: rgb(255, 123, 123);
+  border-radius: 20px;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  margin: 0 5vw 5vw 5vw;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+}
+
+.inc_button:disabled {
+  background-color: rgb(0, 0, 0);
+  color: grey;
+}
+
+.text_no_shadows {
+  font-family: bungee, sans-serif;
+  font-weight: 400;
+  font-size: calc(2rem + 5vw);
+  margin: 0;
+  color: var(--color-primary);
 }
 
 .text_shadows {
@@ -43,15 +134,22 @@ body {
   font-family: bungee, sans-serif;
   font-weight: 400;
   text-transform: uppercase;
-  font-size: calc(2rem + 5vw);
-  text-align: center;
+  font-size: calc(3rem + 20vw);
   margin: 0;
   color: var(--color-primary);
-  //color: transparent;
-  //background-color: white;
-  //background-clip: text;
-  animation: shadows 1.2s ease-in infinite, move 1.2s ease-in infinite;
-  letter-spacing: 0.4rem;
+  color: transparent;
+  background-clip: text;
+  animation: shadows 2s ease-in infinite, move 2s ease-in infinite;
+}
+
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
 }
 
 @keyframes shadows {
