45de52663f44abb428810877eb9d6e886ca58a4f
brleblanc98 <brleblanc98@gmail.com>
2023-04-03 18:35:21 -0400

    Update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,12 +13,3 @@ export default defineNuxtConfig({
         }
     }
 })
-
-// NUXT_FIRE_BASE_API_KEY="AIzaSyCJyjiACBujQ5IYq6RYyHup4ng2HOhZFpE"
-// NUXT_FIRE_BASE_="mackenzie-elliot-ca.firebaseapp.com"
-// NUXT_FIRE_BASE_DATABASE_URL="https://mackenzie-elliot-ca-default-rtdb.firebaseio.com"
-// NUXT_FIRE_BASE_PROJECT_Id="mackenzie-elliot-ca"
-// NUXT_FIRE_BASE_STORAGE_BUCKET="mackenzie-elliot-ca.appspot.com"
-// NUXT_FIRE_BASE_MESSAGING_SENDER_ID="813383392"
-// NUXT_FIRE_BASE_APP_ID="1:813383392:web:d2abc5e2cbb1ee90271a85"
-// NUXT_FIRE_BASE_MEASURMENT_ID="G-0GVMMXZJS2"
